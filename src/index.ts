27e61ea7d40a3879a289533f9a88439781c4a707
Felipe Forbeck <felipe.forbeck@gmail.com>
2025-03-14 15:33:31 -0300

    fix: added helper func to get storage client + tests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-import type { Plugin } from "@elizaos/core";
+import type { ClientInstance, IAgentRuntime, Plugin } from "@elizaos/core";
 import { uploadAction, retrieveAction } from "./actions";
 import { storageClientEnvSchema } from "./environments.ts";
-import { StorageClientInterface } from "./clients/storage.ts";
+import { StorageClientInstanceImpl, StorageClientInterface } from "./clients/storage.ts";
+import * as Storage from '@web3-storage/w3up-client';
+export { StorageClientInterface, StorageClientInstanceImpl as StorageClientImpl } from "./clients/storage.ts";
+
+
+const PluginName = "storage";
 
 export const storagePlugin: Plugin = {
-    name: "storage",
+    name: PluginName,
     description: "Plugin to manage files in a decentralized storage network",
     config: storageClientEnvSchema,
     actions: [uploadAction, retrieveAction],
@@ -13,5 +18,18 @@ export const storagePlugin: Plugin = {
     evaluators: [],
     providers: [],
 };
+
+export const getStorageClient = async (runtime: IAgentRuntime): Promise<StorageClientInstanceImpl> => {
+    const storagePlugin = runtime.plugins.find((plugin) => plugin.name === PluginName);
+    if (storagePlugin && storagePlugin.clients && storagePlugin.clients.length > 0) {
+        const storageStarter = storagePlugin.clients[0];
+        if (storageStarter) {
+            const storageClient = await storageStarter.start(runtime);
+            return storageClient as StorageClientInstanceImpl;
+        }
+    }
+    throw new Error("Storage client not found in runtime");
+}
+
 export default storagePlugin;
 
